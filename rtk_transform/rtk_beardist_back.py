beb02a8c3d0d88e5d8f18ea458975ed8f3df2412
Trim Bresilla <trim.bresilla@gmail.com>
2023-09-22 08:45:13 +0200

    chore: Refactor RTK transform and RTKBeardist classes
    - Updated import statements for `rtk_transform/rtk_transform.py` and `rtk_transform/rtk_beardist.py`
    - Renamed `request_stat` function's request parameter in `rtk_transform/rtk_transform.py`
    - Modified `stat_transform` function to use a different value in `rtk_transform/rtk_transform.py`
    - Added timer to call `stat_transform` function every 10 seconds in `rtk_transform/rtk_transform.py`
    - Set `self.static_transform` to the value of `request.utm_pose` in `request_stat` function of `rtk_transform/rtk_transform.py`
    - Added log message for setting the static transform in `rtk_transform/rtk_transform.py`
    - Renamed `Remapper` class to `RTKBeardist` in `rtk_transform/rtk_beardist.py`
    - Modified `tmp_set` and `fix_set` methods in `RTKBeardist` class of `rtk_transform/rtk_beardist.py`
    - Removed `request_stat` method from `RTKBeardist` class in `rtk_transform/rtk_beardist.py`
    - Modified `gps_callback` method in `RTKBeardist` class of `rtk_transform/rtk_beardist.py`
    - Created `RTKServices` class in `rtk_transform/rtk_beardist.py`
    - Modified `tmp_set`, `fix_set`, and `request_stat` methods in `RTKServices` class of `rtk_transform/rtk_beardist.py`
    - Created global variables `fix_position`, `tmp_position`, and `odom_position` in `rtk_transform/rtk_beardist.py`
    - Modified `main` function to create instances of `RTKBeardist`, `RTKServices`, and `MultiThreadedExecutor` in `rtk_transform/rtk_beardist.py`
    - Added a new file `rtk_beardist_back.py` with various calculations and a class `Remapper`

--- rtk_transform/rtk_beardist_back.py
+++ rtk_transform/rtk_beardist_back.py
@@ -5,7 +5,6 @@ from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from example_interfaces.srv import Trigger
 from handy_msgs.msg import Float32Stamped
-from rclpy.executors import MultiThreadedExecutor
 from handy_msgs.srv import WGS, UTM
 
 def distance(coord1, coord2):
@@ -32,35 +31,63 @@ def bearing(coord1, coord2):
     bearing = math.pi - bearing
     return bearing
 
-class RTKBeardist(Node):
-    def __init__(self):
+class Remapper(Node):
+    def __init__(self, args):
         super().__init__("rtk_beardist")
+        self.tmp_position = None
+        self.fix_position = None
+        self.odom_position = None
         self.gps_sub = self.create_subscription(NavSatFix, "/gps/fix", self.gps_callback, 10)
         self.dist_pub = self.create_publisher(Float32Stamped, "/rtk/distance", 10)
         self.angle_pub = self.create_publisher(Float32Stamped, "/rtk/bearing", 10)
         self.odom_pub = self.create_publisher(Odometry, "/rtk/odom", 10)
         self.dot_pub = self.create_publisher(NavSatFix, "/rtk/fix", 10)
+        self.fix_srv = self.create_service(WGS, '/rtk/fix_set', self.fix_set)
+        self.tmp_srv = self.create_service(Trigger, '/rtk/tmp_set', self.tmp_set)
+        self.cli = self.create_client(UTM, '/rtk/map2odom')
+
+    def tmp_set(self, request, response):
+        self.get_logger().info('FIX SET TO CURRENT POSITION')
+        self.fix_position = self.tmp_position
+        response.message = f"FIX SET TO: {self.fix_position.latitude}, {self.fix_position.longitude}"
+        self.request_stat()
+        return response
+
+    def fix_set(self, request, response):
+        self.get_logger().info('FIX SET TO DATUM')
+        self.fix_position = request.wgs_pose
+        response.message = f"FIX SET TO: {self.fix_position.latitude}, {self.fix_position.longitude}"
+        self.request_stat()
+        return response
+    
+    def request_stat(self):
+        if self.odom_position is None: 
+            self.get_logger().info('TRANSFORM NOT AVAILABLE')
+            return
+        cli_request = UTM.Request()
+        cli_request.utm_pose = self.odom_position
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.cli.call_async(cli_request)
 
     def gps_callback(self, msg):
-        global odom_position
-        global fix_position
-        global tmp_position
-        tmp_position = msg
+        self.tmp_position = msg
         dist_msg = Float32Stamped()
         bear_msg = Float32Stamped()
         dist_msg.header = msg.header
         bear_msg.header = msg.header
-        if fix_position is None:
+        if self.fix_position is None:
             dist_msg.data = 0.0
             bear_msg.data = 0.0
             self.dist_pub.publish(dist_msg)
             self.angle_pub.publish(bear_msg)
             return
-        dist = distance((fix_position.latitude, fix_position.longitude), (msg.latitude, msg.longitude))
-        bear = bearing((fix_position.latitude, fix_position.longitude), (msg.latitude, msg.longitude))
+        
+        dist = distance((self.fix_position.latitude, self.fix_position.longitude), (msg.latitude, msg.longitude))
+        bear = bearing((self.fix_position.latitude, self.fix_position.longitude), (msg.latitude, msg.longitude))
         dist_msg.data = dist
         bear_msg.data = bear
-        self.dot_pub.publish(fix_position)
+        self.dot_pub.publish(self.fix_position)
         self.dist_pub.publish(dist_msg)
         self.angle_pub.publish(bear_msg)
 
@@ -76,64 +103,14 @@ class RTKBeardist(Node):
         odom_msg.pose.pose.orientation.z = 0.0
         odom_msg.pose.pose.orientation.w = 1.0
         self.odom_pub.publish(odom_msg)
-        odom_position = odom_msg
-
-
-class RTKServices(Node):
-    def __init__(self):
-        super().__init__("rtk_services")
-        self.fix_srv = self.create_service(WGS, '/rtk/fix_set', self.fix_set)
-        self.tmp_srv = self.create_service(Trigger, '/rtk/tmp_set', self.tmp_set)
-        self.cli = self.create_client(UTM, '/rtk/map2odom')
-
-    def tmp_set(self, request, response):
-        global fix_position
-        global tmp_position
-        self.get_logger().info('FIX SET TO CURRENT POSITION')
-        fix_position = tmp_position
-        response.message = f"FIX SET TO: {fix_position.latitude}, {fix_position.longitude}"
-        self.request_stat()
-        return response
-
-    def fix_set(self, request, response):
-        global fix_position
-        global tmp_position
-        self.get_logger().info('FIX SET TO DATUM')
-        fix_position = request.wgs_pose
-        response.message = f"FIX SET TO: {fix_position.latitude}, {fix_position.longitude}"
-        self.request_stat()
-        return response
-    
-    def request_stat(self):
-        global odom_position
-        while odom_position is None: 
-            self.get_logger().info('TRANSFORM NOT AVAILABLE')
-        cli_request = UTM.Request()
-        cli_request.utm_pose = odom_position
-        while not self.cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
-        self.cli.call_async(cli_request)
-
-fix_position = None
-tmp_position = None
-odom_position = None
+        self.odom_position = odom_msg
 
 def main(args=None):
     rclpy.init(args=args)
-    try:
-        beardist = RTKBeardist()
-        servoces = RTKServices()
-        executor = MultiThreadedExecutor(num_threads=2)
-        executor.add_node(beardist)
-        executor.add_node(servoces)
-        try:
-            executor.spin()
-        finally:
-            executor.shutdown()
-            beardist.destroy_node()
-            servoces.destroy_node()
-    finally:
-        rclpy.shutdown()
+    navfix = Remapper(args=args)
+    rclpy.spin(navfix)
+    rclpy.shutdown()
 
-if __name__ == '__main__':\
+
+if __name__ == '__main__':
     main()
--- rtk_transform/rtk_transform.py
+++ rtk_transform/rtk_transform.py
@@ -5,37 +5,39 @@ from nav_msgs.msg import Odometry
 import tf2_ros
 from tf2_ros import TransformBroadcaster, StaticTransformBroadcaster
 from geometry_msgs.msg import TransformStamped
-from example_interfaces.srv import Trigger
+from handy_msgs.srv import UTM
 
 class Transformerr(Node):
     def __init__(self, args):
         super().__init__("rtk_transform")
+        self.static_transform = Odometry()
         self.tf_dyna_broadcaster = TransformBroadcaster(self)
         self.tf_stat_broadcaster = StaticTransformBroadcaster(self)
         self.odom_sub = self.create_subscription(Odometry, "/rtk/odom", self.dyna_transform, 10)
-        self.srv = self.create_service(Trigger, '/rtk/stat_transform_reset', self.request_stat)
+        self.srv = self.create_service(UTM, '/rtk/map2odom', self.request_stat)
+        self.timer = self.create_timer(10.0, self.stat_transform)
         self.stat_transform()
 
     def request_stat(self, request, response):
-        self.stat_transform()
+        self.static_transform = request.utm_pose
         response.message = "Static transform reseted"
-        response.success = True
-        self.get_logger().info('Static transform reseted')
+        self.stat_transform()
+        self.get_logger().info(f'STATIC TRANSFORM SET')
         return response
 
-    def stat_transform(self, msg=None):
-        msg_value = msg if msg else Odometry()
+    def stat_transform(self):
+        msg = self.static_transform
         stat_t = TransformStamped()
         stat_t.header.stamp = self.get_clock().now().to_msg()
         stat_t.header.frame_id = "map"
         stat_t.child_frame_id = "odom"
-        stat_t.transform.translation.x = msg_value.pose.pose.position.x
-        stat_t.transform.translation.y = msg_value.pose.pose.position.y
-        stat_t.transform.translation.z = msg_value.pose.pose.position.z
-        stat_t.transform.rotation.x = msg_value.pose.pose.orientation.x
-        stat_t.transform.rotation.y = msg_value.pose.pose.orientation.y
-        stat_t.transform.rotation.z = msg_value.pose.pose.orientation.z
-        stat_t.transform.rotation.w = msg_value.pose.pose.orientation.w
+        stat_t.transform.translation.x = msg.pose.pose.position.x
+        stat_t.transform.translation.y = msg.pose.pose.position.y
+        stat_t.transform.translation.z = msg.pose.pose.position.z
+        stat_t.transform.rotation.x = msg.pose.pose.orientation.x
+        stat_t.transform.rotation.y = msg.pose.pose.orientation.y
+        stat_t.transform.rotation.z = msg.pose.pose.orientation.z
+        stat_t.transform.rotation.w = msg.pose.pose.orientation.w
         self.tf_stat_broadcaster.sendTransform(stat_t)
 
     def dyna_transform(self, msg):
